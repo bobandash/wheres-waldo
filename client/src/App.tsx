e71a372f98091d6de5dbf29bb45626bdda3d2127
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-01 17:06:37 -0700

    Fetch server waldos to create header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
 import allPokemon from './assets/pokemon.jpg'
 import styles from './Game.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SelectCircle from './components/SelectCircle';
+import Header from './components/Header';
+import axios from 'axios';
 
 function App() {
   const sampleCoordinate = {
     posX: 0,
     posY: 0
   }
-
   const [isActive, setIsActive] = useState(false);
   const [coordinates, setCoordinate] = useState(sampleCoordinate)
-  const [pokemon, setPokemon] = useState({});
+  const [waldos, setWaldos] = useState([]);
 
   function handleClick(event : React.MouseEvent<HTMLElement>){
     setIsActive(prevActive => !prevActive);
@@ -21,11 +22,23 @@ function App() {
     })
   }
 
+  useEffect(() => {
+    async function getWaldos(){
+      const data = (await axios.get('/api/waldo/pokemon')).data;
+      setWaldos(data);
+    }
+    getWaldos();
+  }, [])
+
   return (
-    <div onClick = {handleClick} className = {styles["img-container"]}>
-      <img src = {allPokemon} />
-      {isActive && <SelectCircle posX = {coordinates.posX} posY = {coordinates.posY} />}
-    </div>
+    <>
+      <Header waldos = {waldos}/>
+      <div onClick = {handleClick} className = {styles["img-container"]}>
+        <img src = {allPokemon} />
+        {isActive && <SelectCircle posX = {coordinates.posX} posY = {coordinates.posY} />}
+      </div>
+    </>
+
   )
 }
 
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,28 @@
+import {FC} from 'react'
+import { WaldoProps } from '../interfaces/waldo_interface'
+import styles from './Header.module.css'
+
+interface HeaderProps {
+  waldos: Array<WaldoProps>
+}
+
+const Header:FC<HeaderProps> = ({waldos}) => {
+  return(
+    <header className = {styles.header}>
+      <h1 className = {styles.logo}>Findus</h1>
+      <div className = {styles["waldo-container"]}>
+        {waldos.map(waldo => (
+          <div className = {styles["find-elements"]} key = {waldo._id}>
+            <div className = {styles["img-container"]}>
+              <img src = {`/api${waldo.image.path}`} alt = {waldo.name}/>
+            </div>
+            <p>{waldo.name}</p>
+          </div>
+        ))}
+      </div>
+      <h1>SCORE:</h1>
+    </header>
+  )
+}
+
+export default Header;
