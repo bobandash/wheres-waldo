144fc626482c45e8c8e561307c1d88eb84961b63
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-02 12:40:27 -0700

    Create backend to for game functionality

--- server/controller/gameController.js
+++ server/controller/gameController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const GameType = require('../models/gameType')
+const GameInstance = require('../models/gameInstance');
+const Waldos = require('../models/waldo');
 
 exports.game_types = asyncHandler(async (req, res, next) => {
   const {gameType} = req.query;
@@ -10,9 +12,53 @@ exports.game_types = asyncHandler(async (req, res, next) => {
     const gameTypes = await GameType.find({}).lean();
     res.json(gameTypes);
   }
-
 })
 
 exports.create_game = asyncHandler(async (req, res, next) => {
+  const {gameType} = req.body;
+  if(gameType !== ''){
+    const currentGameType = await GameType.findOne({gameType: gameType}).exec();
+    const currentGameNumber = (await GameInstance.countDocuments({}).exec()) + 1;
+    const initialWaldoToFind = await Waldos.find({gameType: currentGameType});
+
+    const newGameInstance = new GameInstance({
+      _id: currentGameNumber,
+      gameType: currentGameType,
+      waldoToFindRemaining: initialWaldoToFind
+    })
+    await newGameInstance.save();
+    await newGameInstance.populate('gameType');
+    res.json(newGameInstance);
+  } else {
+    res.send(400).json("Bad data");
+  }
+})
+
+exports.update_score = asyncHandler(async (req, res, next) => {
+  // increment and return score
+  const {gameId} = req.body;
+  const game = await GameInstance.findByIdAndUpdate(gameId, {
+    $inc: {score: 1}
+  }).exec();
+  res.json({score: game.score});
+})
+
+exports.choose_waldo = asyncHandler(async (req, res, next) => {
+  const {gameId, waldo, posX, posY} = req.body;
+  let hasMatch = false;
   
+  const chosenWaldo = await Waldos.findOne({
+    name: waldo
+  }).exec();
+  const {minPixelX, minPixelY, maxPixelX, maxPixelY} = chosenWaldo.pixelPositionRange;
+  if(posX >= minPixelX && posX <= maxPixelX){
+    if(posY >= minPixelY && posY <+ maxPixelY){
+      hasMatch = true;
+    }
+  }
+  const currentGame = await GameInstance.findById(gameId);
+  if(hasMatch){
+    currentGame.waldoToFindRemaining = currentGame.waldoToFindRemaining.filter(currWaldo => currWaldo.name != waldo);
+  }
+  res.json(currentGame);
 })
--- server/routes/game.js
+++ server/routes/game.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const gameController = require('../controller/gameController')
 
 router.get("/gameTypes", gameController.game_types);
+router.post('/start', gameController.create_game)
+router.put('/update-score', gameController.update_score);
+router.put('/choose-waldo', gameController.choose_waldo);
 
 module.exports = router;
