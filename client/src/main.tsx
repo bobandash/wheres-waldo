7b2f31e36a63112ae0992bb18618c5d008dcfa1c
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-13 14:44:43 -0800

    Fix typescript error

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Router from "./Router";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
