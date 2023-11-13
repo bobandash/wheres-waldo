5b7eaedf66d3775ed0063bed6f762a20295e63f8
Bruce Hsu <brucehsu1126@gmail.com>
2023-11-12 16:42:10 -0800

    Add loading screen to initial page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import GamemodeOptions from "./components/GamemodeOptions";
 import { GameTypeProps } from "./interfaces/game_type_interface";
+import LoadingScreen from "./pages/Loading/LoadingScreen";
 
 const App = () => {
   const [gameTypes, setGameTypes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     async function getGameTypes() {
       const data = (await axios.get("/api/game/gameTypes")).data;
       setGameTypes(data);
     }
     getGameTypes();
+    setIsLoading(false);
   }, []);
+
+  if(isLoading){
+    return <LoadingScreen />
+  }
 
   return (
     <div className={styles.container}>
